ff3ecaa12aad2d5ca0e26ae75bfe5021baefc670
Brenda <bt@gmail.com>
2021-12-01 11:01:55 +0100

    Branch de Livio

--- src/ch/hegarc/ig/Main.java
+++ src/ch/hegarc/ig/Main.java
@@ -3,6 +3,6 @@ package ch.hegarc.ig;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("C la branche de Seb");
+        System.out.println("C la branche de Livio");
     }
 }
