b1c7e4c425332cb631965b3aedf80755a857dc94
niklas.neels <niklas.neels@he-arc.ch>
2021-12-01 11:01:00 +0100

    test

--- src/ch/hegarc/ig/Main.java
+++ src/ch/hegarc/ig/Main.java
@@ -3,6 +3,6 @@ package ch.hegarc.ig;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("C la branche de Niklas");
+        System.out.println("C la branche de NiklasS ");
     }
 }
