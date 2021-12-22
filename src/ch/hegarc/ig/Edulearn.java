7fc56a82ade6fe8e0a42f351a05746ac4619fc16
Sebklay <sebklay@hotmail.com>
2021-12-22 08:49:18 +0100

    Ajout de la classe Apprenant et implémentation des fonctions

--- src/ch/hegarc/ig/Edulearn.java
+++ src/ch/hegarc/ig/Edulearn.java
@@ -1,17 +1,46 @@
 package ch.hegarc.ig;
 
+import ch.hegarc.ig.business.Apprenant;
 import ch.hegarc.ig.business.Chapitre;
 import ch.hegarc.ig.business.Cours;
 import ch.hegarc.ig.business.Document;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Edulearn implements IEdulearn{
+    List<Apprenant> apprenants = new ArrayList<>();
+    List<Document> documents = new ArrayList<>();
+
     @Override
     public boolean supprimerDeListe(String matricule) {
+        for (int i = 0; i <= apprenants.size(); i++){
+            if (apprenants.get(i).getMatricule().equals(matricule)){
+                apprenants.remove(i);
+                return true;
+            }else {
+                return false;
+            }
+        }
+
         return false;
     }
 
     @Override
     public boolean ajouterDocument(Cours cours, Document docu, Chapitre chap) {
-        return false;
+        if (cours != null){
+            if (chap != null){
+                documents.add(docu);
+                return true;
+            }else {
+                System.out.println("Le chapitre souhaité n'existe pas...");
+
+                return false;
+            }
+        }else {
+            System.out.println("Le cours souhaité n'existe pas");
+
+            return false;
+        }
     }
 }
---
+++ src/ch/hegarc/ig/business/Apprenant.java
@@ -0,0 +1,17 @@
+package ch.hegarc.ig.business;
+
+public class Apprenant {
+    private String matricule;
+
+    public Apprenant(String matricule) {
+        this.matricule = matricule;
+    }
+
+    public String getMatricule() {
+        return matricule;
+    }
+
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
+    }
+}
