28c0302d50668944739fb4429dba24889092d3f1
NiklasNeels <niklas.neels@he-arc.ch>
2021-12-22 08:42:54 +0100

    consulterListeClasse et creerCours

---
+++ src/ch/hegarc/ig/business/Apprenant.java
@@ -0,0 +1,14 @@
+package ch.hegarc.ig.business;
+
+import java.util.List;
+
+public class Apprenant extends Personne{
+
+    public Apprenant(String nom, String prenom) {
+        super(nom, prenom);
+    }
+
+    public Apprenant(String nom, String prenom, String matricule, Formation formation, List<Cours> coursSuivis) {
+        super(nom, prenom, matricule, formation);
+    }
+}
---
+++ src/ch/hegarc/ig/edulearn/IEduLearn.java
@@ -0,0 +1,16 @@
+package ch.hegarc.ig.edulearn;
+
+import ch.hegarc.ig.business.*;
+
+import java.util.List;
+
+public interface IEduLearn {
+
+    Personne authentification(String username, String pwd);
+
+    boolean creerCours(String nom, Matiere matière, String commentaire, Enseignant proprietaire);
+
+    boolean quitterCours(Apprenant apprenant, Cours cours);
+
+    List<Apprenant> consulterListeClasse(Enseignant enseignant, Cours cours);
+}
